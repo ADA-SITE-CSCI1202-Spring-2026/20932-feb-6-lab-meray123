cb2af8791a4ff84c52a4d573fc353045812906af
meray123 <mmustafayeva16954@ada.edu.az>
2026-02-06 11:09:05 +0400

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int b = input.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+
+        input.close();
+    }
+}
