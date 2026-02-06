d3a5917d0d34281fb67acf38706a53989367364d
meray123 <mmustafayeva16954@ada.edu.az>
2026-02-06 11:09:30 +0400

    Create time.java

---
+++ time.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Time {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter total seconds: ");
+        int totalSeconds = input.nextInt();
+
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        System.out.println("Hours: " + hours);
+        System.out.println("Minutes: " + minutes);
+        System.out.println("Seconds: " + seconds);
+
+        input.close();
+    }
+}
